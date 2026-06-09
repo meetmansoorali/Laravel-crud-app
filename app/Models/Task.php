61d27d47b0e9907b955685fd9dbacb0ab9d107ff
MansoorAli <mansoorali.hey.mansoorali@gmail.com>
2026-06-10 01:56:37 +0500

    feat: create Task model and tasks migration schema

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public function up(): void
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->text('city')->nullable();
+        $table->timestamps();
+    });
+}
+}
