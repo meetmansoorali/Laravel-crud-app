78caca34b4d7108495590a8844a842e62636afcb
MansoorAli <mansoorali.hey.mansoorali@gmail.com>
2026-06-10 10:04:51 +0500

    setuped welcome page's route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [UserController::class, 'show']);
